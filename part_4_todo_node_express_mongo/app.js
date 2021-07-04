df9d8ddc1f0f4d4fab3c0b7fc36e9398393a31f7
displaygreat <displaygreat@gmail.com>
2021-07-04 14:33:46 +0300

    setup mongodb, model and schema, set and get data

--- part_4_todo_node_express_mongo/app.js
+++ part_4_todo_node_express_mongo/app.js
@@ -1,7 +1,41 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const config = require("./config");
+const mongodb = config.MONGODB_CONNECTION;
+console.log(mongodb);
+const Item = require("./models/items");
+mongoose
+  .connect(mongodb, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("connected");
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
 app.set("view engine", "ejs");
-app.listen(3000);
+
+app.get("/create-item", (req, res) => {
+  const item = new Item({
+    name: "headphones",
+    price: 500,
+  });
+  item
+    .save()
+    .then((result) => res.send(result))
+    .catch((err) => console.log(err));
+});
+
+app.get("/get-items", (req, res) => {
+  Item.find()
+    .then((result) => res.send(result))
+    .catch((err) => console.log(err));
+});
+
+app.get("/get-item", (req, res) => {
+  Item.findById("60e0ca096b98601cb45f7155")
+    .then((result) => res.send(result))
+    .catch((err) => console.log(err));
+});
 
 app.get("/", function (req, res) {
   const items = [
